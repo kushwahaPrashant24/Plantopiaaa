d4ba4b00ec23cbbef696065f234695875ce75129
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-01-23 22:15:15 +0530

    added Homepage and debug the css of Navbar

--- plantopia-front/src/App.js
+++ plantopia-front/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
- 
 import Header from './Navbar/Header';
+import Content from './Content/content'
 
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Content />
      
     </div>
   );
---
+++ plantopia-front/src/Content/homepage.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Homepage(){
+const [image, setImage] = useState(0);
+
+    return <div>
+
+    </div>
+}
+
+export default Homepage;
