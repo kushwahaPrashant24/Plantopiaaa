663da43deddd04cddca72fa01970cbebfcb38b06
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-01-24 15:46:44 +0530

    added Offer section

---
+++ plantopia-front/src/Content/combo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Combo() {
+    return <div>
+        <h2><center>Value For Money - COMBO OFFER</center></h2>
+    </div>
+}
+
+export default Combo;
--- plantopia-front/src/Content/content.jsx
+++ plantopia-front/src/Content/content.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Homepage from "./homepage";
+import Combo from "./combo";
 
 function  Content(){
     return <div><Homepage />
+    <Combo />
     </div>
 }
 
