e09e1d50260df017c4fc9e5e7791008a80dbebc7
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-01-27 23:09:55 +0530

    added styling in offer section

--- plantopia-front/src/Content/offer/offer.jsx
+++ plantopia-front/src/Content/offer/offer.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-import './offer.css';
+import "./offer.css";
+import image from "../../Images/air purify plant.jpg";
 
 function Offer() {
-    return 
+  return (
+    <div id="offer">
+      <div className="offer-main">
+        <div className="offer-one">
+          <img className="one" src={image} />
+        </div>
+        <div className="offer-two">
+          <h1>NASA</h1>
+          <h3>RECOMMENDED</h3>
+          <h2>AIR PURIFY PLNTS</h2>
+          <div className="discount">
+          <span>UP TO</span>
+          <p>50%</p><span>OFF</span>
+          </div>
+        </div>
+      </div>
+      <div className="offer-main">
+        <div className="offer-one">
+          <img className="one" src={image} />
+        </div>
+        <div className="offer-two"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Offer;
