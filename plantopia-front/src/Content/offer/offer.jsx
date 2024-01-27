f1af4a3a6b350f755447760fc83d0f78e5314af8
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-01-28 01:22:38 +0530

    completed the offer section

--- plantopia-front/src/Content/offer/offer.jsx
+++ plantopia-front/src/Content/offer/offer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./offer.css";
+import Button from '@mui/material/Button';
 import image from "../../Images/air purify plant.jpg";
 
 function Offer() {
@@ -10,12 +11,21 @@ function Offer() {
           <img className="one" src={image} />
         </div>
         <div className="offer-two">
-          <h1>NASA</h1>
-          <h3>RECOMMENDED</h3>
-          <h2>AIR PURIFY PLNTS</h2>
+          <h1 className="offer-head1">NASA</h1>
+          <h3 className="offer-head3">RECOMMENDED</h3>
+          <h2 className="offer-head2">AIR PURIFY PLNTS</h2>
           <div className="discount">
-          <span>UP TO</span>
-          <p>50%</p><span>OFF</span>
+
+            <span>UP TO</span>
+            <div className="offer-data">
+            <p>50%</p><span>OFF</span>
+            
+            </div>
+            <div id="offer-button">
+            <Button  className="offer-button" variant="contained" color="success">
+       SHOP NOW
+    </Button>
+    </div>
           </div>
         </div>
       </div>
@@ -23,7 +33,17 @@ function Offer() {
         <div className="offer-one">
           <img className="one" src={image} />
         </div>
-        <div className="offer-two"></div>
+        <div className="offer-two" id="two-offer">
+        <h1 className="offer-head1">SUMMER</h1>
+        <p>VEGETABLE SEEDS</p>
+        <p>FLAT 50% OFF</p>
+        <p>Extra 5%off on shopping 599/-</p>
+        
+        <Button  className="offer-button"   variant="contained" color="success">
+       SHOP NOW
+    </Button>
+   
+        </div>
       </div>
     </div>
   );
