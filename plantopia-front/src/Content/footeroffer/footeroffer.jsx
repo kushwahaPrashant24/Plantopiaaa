48f6be643b2a1af73fa6fe15f180510ab9dfa7bd
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-01-29 10:13:36 +0530

    did proper componenting and created another offer section

---
+++ plantopia-front/src/Content/footeroffer/footeroffer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './footeroffer.css';
+import Pabble from '../../Images/pa.jpeg'
+
+function Footeroffer() {
+    return  <div id="footeroffer">
+        <img id="Pabble" src={Pabble} />
+    </div>
+}
+
+export default Footeroffer;
