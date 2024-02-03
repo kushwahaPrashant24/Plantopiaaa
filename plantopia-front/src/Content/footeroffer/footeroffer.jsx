605efff016f9512c14a17121fe1f266f2868a582
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-02-03 15:11:34 +0530

    completed the footeroffer

--- plantopia-front/src/Content/footeroffer/footeroffer.jsx
+++ plantopia-front/src/Content/footeroffer/footeroffer.jsx
@@ -5,13 +5,14 @@ import Button from '@mui/material/Button';
 
 function Footeroffer() {
     return  <div id="footeroffer">
-     <div id="Pabble-data">
-        <h1>Pabble Start Rs. 59</h1>
-        <p>Add visiual and texture feature to your garden with wide range of naturel pabbles</p>
-        <Button variant="contained" color="success">Contained</Button>
-        </div>
+    
         <div>
         <img id="Pabble" src={Pabble} />
+        </div>
+        <div id="Pabble-data">
+        <h1>Pabble Start Rs. 59</h1>
+        <div><p>Add visiual and texture feature to your garden with wide range of naturel pabbles</p></div>
+        <Button variant="contained" color="success">Contained</Button>
         </div>
     </div>
 }
