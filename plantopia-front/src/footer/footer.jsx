4b86221b3449b41dc42ade7722adbac0503ebb84
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-02-03 17:29:06 +0530

    added boxing in Footer

---
+++ plantopia-front/src/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './footer.css';
+import FooterHead from '../footer/footerhead/footerhead'
+
+function Footer() {
+    return <div id="Footer">
+        <FooterHead />
+    </div>
+}
+
+export default Footer;
